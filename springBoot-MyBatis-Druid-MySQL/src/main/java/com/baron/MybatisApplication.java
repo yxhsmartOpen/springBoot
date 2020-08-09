2410bf92cf0323fc027451fe8395df22e1d204f8
yangxuanhua <714841699>
2020-08-09 22:10:39 +0800

    modify some configruation

--- springBoot-MyBatis-Druid-MySQL/src/main/java/com/baron/MybatisApplication.java
+++ springBoot-MyBatis-Druid-MySQL/src/main/java/com/baron/MybatisApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MybatisApplication {
 
     public static void main(String[] args) {
-        System.out.println("1111");
+
         SpringApplication.run(MybatisApplication.class, args);
     }
 }
