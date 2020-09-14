c944327be407c60fb8fa030b92fbf2a91754a53d
yangxuanhua <714841699>
2020-09-14 22:26:19 +0800

    modify some configruation

--- springBoot-MyBatis-Druid-MySQL/src/main/java/com/baron/weather/enums/CharacterPinYinConvert.java
+++ springBoot-MyBatis-Druid-MySQL/src/main/java/com/baron/weather/enums/CharacterPinYinConvert.java
@@ -14,14 +14,15 @@ import java.util.Set;
  * @date 2020/9/13 6:53
  */
 public class CharacterPinYinConvert {
-    /** 设置汉字拼音输出的格式 **/
+    /**
+     * 设置汉字拼音输出的格式
+     **/
     HanyuPinyinOutputFormat format = null;
 
     /**
      * 默认构造方法，初始化汉字拼音输出格式
      */
-    public CharacterPinYinConvert()
-    {
+    public CharacterPinYinConvert() {
         // 设置汉字拼音输出的格式
         format = new HanyuPinyinOutputFormat();
         // 不使用音调标记，
@@ -39,38 +40,28 @@ public class CharacterPinYinConvert {
     /**
      * 汉字转拼音
      *
-     * @param text
-     *           待转换字符串
+     * @param text 待转换字符串
      * @return allPinYin
      */
-    public String toPinYin(String text)
-    {
+    public String toPinYin(String text) {
         StringBuilder allPinYin = new StringBuilder();
-        for (int i = 0; i < text.length(); i++)
-        {
+        for (int i = 0; i < text.length(); i++) {
             char wordChar = text.charAt(i);
             // 如果为汉字
-            if (Character.toString(wordChar).matches("[\\u4E00-\\u9FA5]"))
-            {
-                try
-                {
+            if (Character.toString(wordChar).matches("[\\u4E00-\\u9FA5]")) {
+                try {
                     // 返回汉字的全部拼音(因为有些汉字为多音字，否则只返回一个)
                     String[] pinYinArray = PinyinHelper.toHanyuPinyinStringArray(wordChar, format);
                     // 取第一个拼音
                     String pinYin = pinYinArray[0];
 
-                    if (pinYin != null)
-                    {
+                    if (pinYin != null) {
                         // 把拼音的首字母改为大写，例如：BeiJing
-                        allPinYin.append(pinYin.substring(0,1).toUpperCase()).append(pinYin.substring(1,pinYin.length()));
+                        allPinYin.append(pinYin.substring(0, 1).toUpperCase()).append(pinYin.substring(1, pinYin.length()));
                     }
+                } catch (Exception e) {
                 }
-                catch (Exception e)
-                {
-                }
-            }
-            else if (((int) wordChar >= 65 && (int) wordChar <= 90) || ((int) wordChar >= 97 && (int) wordChar <= 122))
-            {
+            } else if (((int) wordChar >= 65 && (int) wordChar <= 90) || ((int) wordChar >= 97 && (int) wordChar <= 122)) {
                 allPinYin.append(wordChar);
             }
         }
@@ -80,39 +71,29 @@ public class CharacterPinYinConvert {
     /**
      * 汉字转拼音
      *
-     * @param text
-     *           待转换字符串
+     * @param text 待转换字符串
      * @return pinYinSet
      */
-    public Set<String> toPinYins(String text)
-    {
+    public Set<String> toPinYins(String text) {
         char[] srcChar = text.toCharArray();
 
         String[][] temp = new String[text.length()][];
-        for (int i = 0; i < srcChar.length; i++)
-        {
+        for (int i = 0; i < srcChar.length; i++) {
             char c = srcChar[i];
-            if (String.valueOf(c).matches("[\\u4E00-\\u9FA5]"))
-            {
+            if (String.valueOf(c).matches("[\\u4E00-\\u9FA5]")) {
                 try {
-                    temp[i] = PinyinHelper.toHanyuPinyinStringArray(srcChar[i],format);
+                    temp[i] = PinyinHelper.toHanyuPinyinStringArray(srcChar[i], format);
+                } catch (Exception e) {
                 }
-                catch (Exception e) {
-                }
-            }
-            else if (((int) c >= 65 && (int) c <= 90)|| ((int) c >= 97 && (int) c <= 122))
-            {
-                temp[i] = new String[] { String.valueOf(srcChar[i]) };
-            }
-            else
-            {
-                temp[i] = new String[] { "" };
+            } else if (((int) c >= 65 && (int) c <= 90) || ((int) c >= 97 && (int) c <= 122)) {
+                temp[i] = new String[]{String.valueOf(srcChar[i])};
+            } else {
+                temp[i] = new String[]{""};
             }
         }
         String[] pingyinArray = doExchange(temp)[0];
         Set<String> pinyinSet = new HashSet<String>();
-        for (int i = 0; i < pingyinArray.length; i++)
-        {
+        for (int i = 0; i < pingyinArray.length; i++) {
             pinyinSet.add(pingyinArray[i]);
         }
         return pinyinSet;
@@ -123,40 +104,32 @@ public class CharacterPinYinConvert {
      *
      * @return
      */
-    private String[][] doExchange(String[][] strJaggedArray)
-    {
+    private String[][] doExchange(String[][] strJaggedArray) {
         int len = strJaggedArray.length;
-        if (len >= 2)
-        {
+        if (len >= 2) {
             int len1 = strJaggedArray[0].length;
             int len2 = strJaggedArray[1].length;
             int newlen = len1 * len2;
             String[] temp = new String[newlen];
             int index = 0;
-            for (int i = 0; i < len1; i++)
-            {
-                for (int j = 0; j < len2; j++)
-                {
+            for (int i = 0; i < len1; i++) {
+                for (int j = 0; j < len2; j++) {
                     temp[index] = strJaggedArray[0][i] + strJaggedArray[1][j];
                     index++;
                 }
             }
             String[][] newArray = new String[len - 1][];
-            for (int i = 2; i < len; i++)
-            {
+            for (int i = 2; i < len; i++) {
                 newArray[i - 1] = strJaggedArray[i];
             }
             newArray[0] = temp;
             return doExchange(newArray);
         }
-        else
-        {
-            return strJaggedArray;
-        }
+
+        return strJaggedArray;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         CharacterPinYinConvert convert = new CharacterPinYinConvert();
         String name = "北京";
         System.out.println(convert.toPinYin(name));
